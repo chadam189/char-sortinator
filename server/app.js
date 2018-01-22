2e8c4c5e963c04a4cf0c4d26a4fbc5c3ea97fa16
Adam Miller <chadam189@gmail.com>
2018-01-22 14:14:17 -0800

    Install body parser, build out skeleton of routes

--- server/app.js
+++ server/app.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.JSON());
+
 app.get('/', (req, res) => {
   // serve up static files
+});
+
+app.post('/sortinate', (req, res) => {
+	// get string 
+	// send it to helper function to sort string
+	// return result
+});
+
+app.all('*', (req, res) => {
+	res.send(404);
 });
 
 app.listen(3000, () => console.log('Char Sortinator is listening on port 3000!'));
