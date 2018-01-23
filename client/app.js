95d8b35bade4481d6c8a38d1b481973bccbec76c
Adam Miller <chadam189@gmail.com>
2018-01-22 17:21:05 -0800

    Refactor front-end input handling and fetch call, and sortinate route

--- client/app.js
+++ client/app.js
@@ -1,18 +1,44 @@
-$(document).ready(() => {
-	$('.sortinate')
-	  .submit((event) => {
-	  	const message = {
-	  		unsorted: $('input[name=unsorted').val()
-	  	};
+// $(document).ready(() => {
+// 	$('.sortinate')
+// 	  .submit((event) => {
+// 	  	const message = {
+// 	  		unsorted: $('input[name=unsorted').val()
+// 	  	};
 
-	  	$.ajax({
-	  		url: $form.attr('action').
-	  		data: message,
-	  		dataType: 'json',
-	  		type: 'POST',
-	  	}).done((data) => {
-	  		console.log(data);
-	  	});
-	  	event.preventDefault();
-	  });
-});
+// 	  	$.ajax({
+// 	  		url: $form.attr('action').
+// 	  		data: message,
+// 	  		dataType: 'json',
+// 	  		type: 'POST',
+// 	  	}).done((data) => {
+// 	  		console.log(data);
+// 	  	});
+// 	  	event.preventDefault();
+// 	  });
+// });
+
+// const $ = require('jquery');
+
+const submitString = () => {
+
+	const url = 'http://localhost:3000/sortinate';
+	const unsorted = document.getElementsByClassName('unsorted')[0].value;
+
+	const message = { 
+		unsorted,
+	};
+
+	fetch(url, {
+		method: 'POST',
+		body: JSON.stringify(message),
+		headers: new Headers({
+			'Content-Type': 'application/json'
+		})
+	}).then((response) => {
+	  	console.log(`response: ${response}`);
+	  	return response.json()
+	  })
+	  .catch(error => console.log(`Error: ${error}`))
+	  .then(result => console.log(`Success: ${JSON.stringify(result)}`));
+};
+
