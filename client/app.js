4528673b6d828d5a048ea8b3e3700be3d4282af5
Adam Miller <chadam189@gmail.com>
2018-01-22 18:35:22 -0800

    Clean up code (delete old comments)

--- client/app.js
+++ client/app.js
@@ -1,24 +1,3 @@
-// $(document).ready(() => {
-// 	$('.sortinate')
-// 	  .submit((event) => {
-// 	  	const message = {
-// 	  		unsorted: $('input[name=unsorted').val()
-// 	  	};
-
-// 	  	$.ajax({
-// 	  		url: $form.attr('action').
-// 	  		data: message,
-// 	  		dataType: 'json',
-// 	  		type: 'POST',
-// 	  	}).done((data) => {
-// 	  		console.log(data);
-// 	  	});
-// 	  	event.preventDefault();
-// 	  });
-// });
-
-// const $ = require('jquery');
-
 const submitString = () => {
 
 	const url = 'http://localhost:3000/sortinate';
